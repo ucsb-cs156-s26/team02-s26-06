1084ef11cb9e3093582970aeee85611db3d36a4b
Mr465truck <evoncolln@ucsb.edu>
2026-05-01 19:22:41 -0700

    add UCSBDiningCommonMenuItemForm, stories, and tests. update fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+  oneMenuItem: {
+      id: 1,
+      diningCommonsCode: "ortega",
+      name: "chicken",
+      station: "entrees",
+    },
+
+  threeMenuItems: [
+    {
+      id: 1,
+      diningCommonsCode: "ortega",
+      name: "chicken",
+      station: "entrees",
+    },
+    {
+      id: 2,
+      diningCommonsCode: "portola",
+      name: "pizza",
+      station: "entrees",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "dlg",
+      name: "brownie",
+      station: "desserts",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
