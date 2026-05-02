47f2c3927ed8093cac626b45cc22e367ff550114
Prisha Bobde <prishabobde@ucsb.edu>
2026-05-02 11:31:02 -0700

    pb - WIP fix formatting

--- frontend/src/main/components/Articles/ArticleForm.jsx
+++ frontend/src/main/components/Articles/ArticleForm.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router";
 function ArticleForm({
   initialContents,
   submitAction,
-  buttonLabel = "Create"
+  buttonLabel = "Create",
 }) {
   // Stryker disable all
   const {
