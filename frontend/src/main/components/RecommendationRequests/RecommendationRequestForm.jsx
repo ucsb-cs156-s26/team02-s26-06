2e4a2464c280856ebc2cb33733d7a3d816e5f6cb
Dongyi Xia <dongyi_xia@ucsb.edu>
2026-05-04 11:31:37 -0700

    dx - WIP two fields

---
+++ frontend/src/main/components/RecommendationRequests/RecommendationRequestForm.jsx
@@ -0,0 +1,109 @@
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+
+function RecommendationRequestForm({
+  initialContents,
+  submitAction,
+  buttonLabel = "Create",
+}) {
+  // Stryker disable all
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ defaultValues: initialContents || {} });
+  // Stryker restore all
+
+  const navigate = useNavigate();
+
+  const testIdPrefix = "RecommendationRequestForm";
+
+  return (
+    <Form onSubmit={handleSubmit(submitAction)}>
+      {initialContents && (
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="id">Id</Form.Label>
+          <Form.Control
+            data-testid={testIdPrefix + "-id"}
+            id="id"
+            type="text"
+            {...register("id")}
+            value={initialContents.id}
+            disabled
+          />
+        </Form.Group>
+      )}
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="requesterEmail">RequesterEmail</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-requesterEmail"}
+          id="requesterEmail"
+          type="text"
+          isInvalid={Boolean(errors.requesterEmail)}
+          {...register("requesterEmail", {
+            required: "RequesterEmail is required.",
+            maxLength: {
+              value: 255,
+              message: "Max length 255 characters",
+            },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.requesterEmail?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+      
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="professorEmail">ProfessorEmail</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-professorEmail"}
+          id="professorEmail"
+          type="text"
+          isInvalid={Boolean(errors.professorEmail)}
+          {...register("professorEmail", {
+            required: "ProfessorEmail is required.",
+            maxLength: {
+              value: 255,
+              message: "Max length 255 characters",
+            },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.professorEmail?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="description">Description</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-description"}
+          id="description"
+          type="text"
+          isInvalid={Boolean(errors.description)}
+          {...register("description", {
+            required: "Description is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.description?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Button type="submit" data-testid={testIdPrefix + "-submit"}>
+        {buttonLabel}
+      </Button>
+      <Button
+        variant="Secondary"
+        onClick={() => navigate(-1)}
+        data-testid={testIdPrefix + "-cancel"}
+      >
+        Cancel
+      </Button>
+    </Form>
+  );
+}
+
+export default RecommendationRequestForm;
