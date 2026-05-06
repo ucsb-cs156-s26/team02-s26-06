81a579fa54c3977c0b7885a8d945c67ea20396de
Mr465truck <evoncolln@ucsb.edu>
2026-05-05 17:19:45 -0700

    Revert "fix wrong api endpoint"
    This reverts commit ff3db7daa96fad479f09121d0f9f717a71003e85.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -137,7 +137,7 @@ function App() {
         <>
           <Route
             exact
-            path="/`diningcommonsmenuitem`"
+            path="/ucsbdiningcommonsmenuitem"
             element={<UCSBDiningCommonsMenuItemIndexPage />}
           />
         </>
@@ -146,12 +146,12 @@ function App() {
         <>
           <Route
             exact
-            path="/diningcommonsmenuitem/edit/:id"
+            path="/ucsbdiningcommonsmenuitem/edit/:id"
             element={<UCSBDiningCommonsMenuItemEditPage />}
           />
           <Route
             exact
-            path="/diningcommonsmenuitem/create"
+            path="/ucsbdiningcommonsmenuitem/create"
             element={<UCSBDiningCommonsMenuItemCreatePage />}
           />
         </>
