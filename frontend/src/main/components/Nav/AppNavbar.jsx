88d0ddf2626d259ef32b91b4eebc157501f9e2ab
Aiden Hunter <aidenhunter@ucsb.edu>
2026-05-05 17:29:40 -0700

    Review my PRS first so i dont need to do merge conflcits pls thanjksgit add .git add . jk

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -82,6 +82,7 @@ export default function AppNavbar({
                   </Nav.Link>
                   <Nav.Link as={Link} to="/menuitemreview">
                     Menu Item Reviews
+                  </Nav.Link>
                   <Nav.Link as={Link} to="/helprequests">
                     Help Requests
                   </Nav.Link>
