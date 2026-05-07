5dc5f56f71cce166cc1d1b862101eea92f200058
Aiden Hunter <aidenhunter@ucsb.edu>
2026-05-06 20:20:22 -0700

    test

---
+++ frontend/src/main/pages/MenuItemReview/MenuItemReviewCreatePage.jsx
@@ -0,0 +1,51 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import MenuItemReviewForm from "main/components/MenuItemReviews/MenuItemReviewForm";
+import { Navigate } from "react-router";
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
+
+export default function MenuItemReviewCreatePage({ storybook = false }) {
+  const objectToAxiosParams = (menuItemReview) => ({
+    url: "/api/menuitemreview/post",
+    method: "POST",
+    params: {
+      itemId: menuItemReview.itemId,
+      reviewerEmail: menuItemReview.reviewerEmail,
+      stars: menuItemReview.stars,
+      comments: menuItemReview.comments,
+      dateReviewed: menuItemReview.dateReviewed,
+    },
+  });
+
+  const onSuccess = (menuItemReview) => {
+    toast(
+      `New Menu Item Review Created - itemId: ${menuItemReview.itemId} reviewerEmail: ${menuItemReview.reviewerEmail} stars: ${menuItemReview.stars} comments: ${menuItemReview.comments} dateReviewed: ${menuItemReview.dateReviewed}`,
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    ["/api/menuitemreview/all"],
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/menuitemreview" />;
+  }
+
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Create New Menu Item Review</h1>
+        <MenuItemReviewForm submitAction={onSubmit} />
+      </div>
+    </BasicLayout>
+  );
+}
